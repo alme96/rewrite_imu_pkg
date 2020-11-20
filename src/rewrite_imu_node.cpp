2ed480969723553a224152734def379f0c73b038
ale <alme96@hotmail.it>
2020-11-20 10:20:50 +0100

    update imu node

--- src/rewrite_imu_node.cpp
+++ src/rewrite_imu_node.cpp
@@ -3,17 +3,18 @@
 #include "boost/bind.hpp"
 
 
-void imuCallback(const sensor_msgs::Imu::ConstPtr& msg)
+void imuCallback(const sensor_msgs::Imu::ConstPtr& msg, const ros::Publisher pub)
 {
   sensor_msgs::Imu new_imu;
   //Update covariance matrices in order to pass the filter
-  new_imu.angular_velocity_covariance[0] = 0.01;
-  new_imu.linear_acceleration_covariance[0]= 0.01;
-  new_imu.orientation_covariance[0] = -1;
+  new_imu.angular_velocity_covariance = {0.01, 0.0, 0.0, 0.0, 0.01, 0.0, 0.0, 0.0, 0.01};
+  new_imu.linear_acceleration_covariance = {0.01, 0.0, 0.0, 0.0, 0.01, 0.0, 0.0, 0.0, 0.01};
   //Copy the measurements from the old imu to the new imu message
   new_imu.header = msg->header;
   new_imu.angular_velocity = msg->angular_velocity;
   new_imu.linear_acceleration = msg->linear_acceleration;
+  new_imu.orientation = msg->orientation;
+  new_imu.orientation_covariance = msg->orientation_covariance;
 
   pub.publish(new_imu);
 }
